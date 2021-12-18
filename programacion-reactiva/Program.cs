be838a98de1758da44345fd129b1ab225ba15058
jafb.dev <jafb.dev@DESKTOP-AM1Q3LO>
2021-12-17 18:16:45 -0700

    Agregar archivos de proyecto.

---
+++ programacion-reactiva/Program.cs
@@ -0,0 +1,15 @@
+﻿using programacion_reactiva;
+
+myObservable observable = new myObservable();
+
+observable.subscribe(some => {
+    Console.WriteLine("Look! the object did something");
+});
+
+observable.subscribe(some => {
+    Console.WriteLine("Oh, yea, I saw it too");
+});
+
+observable.doSomething();
+observable.doSomething();
+observable.doSomething();
---
+++ programacion-reactiva/myObservable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive;
+
+namespace programacion_reactiva
+{
+    // Personal implementation
+    // hdeleon implementation https://www.youtube.com/watch?v=Kq0PNa-BWKU&ab_channel=hdeleon.net
+    internal class myObservable
+    {
+        public event EventHandler eventHandler;
+        public IObservable<object> observable;
+        
+        public myObservable()
+        { 
+            observable = Observable.FromEventPattern(
+                    e => eventHandler += e,
+                    e => eventHandler -= e
+                );
+
+        }
+        
+        public void doSomething()
+        {
+            eventHandler(null, EventArgs.Empty);
+        }
+
+        public void subscribe(Action<object> action)
+        {
+            observable.Subscribe(action);
+        }
+    }
+}
